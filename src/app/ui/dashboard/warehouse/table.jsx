30b1ef4134c60d54d5eb6207fe0aceaaf2938ffe
EricJurbergs <erj0008@uah.edu>
2024-11-29 23:59:16 -0600

    Removed some unnecessary code from warehouse table

--- src/app/ui/dashboard/warehouse/table.jsx
+++ src/app/ui/dashboard/warehouse/table.jsx
@@ -8,41 +8,6 @@ export default async function WarehouseTable(query) {
     <div className="mt-6 flow-root">
         <div className="inline-block min-w-full align-middle">
             <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
-                <div className="md:hidden">
-                    {
-                        warehouseItems?.map((item) => (
-                        <div
-                            key={item._id}
-                            className="mb-2 w-full rounded-md bg-white p-4"
-                        >
-                            <div className="flex items-center justify-between border-b pb-4">
-                                <div>
-                                    <div className="mb-2 flex items-center">
-                                        <p>{item.name}</p>
-                                    </div>
-                                    <p className="text-sm text-gray-500">{item.category}</p>
-                                </div>
-                            </div>
-                            <div className="flex w-full items-center justify-between pt-4">
-                                <div>
-                                    <p className="text-xl font-medium">
-                                        {item.price
-                                        }
-                                    </p>
-                                    <p>{item.stock
-                                    }</p>
-                                    <p>
-                                        {item.description
-                                        }
-                                    </p>
-                                </div>
-                                <div className="flex justify-end gap-2">
-                                </div>
-                            </div>
-                        </div>
-                    ))
-                    }
-                </div>
                 <table className="hidden min-w-full text-gray-900 md:table">
                     <thead className="rounded-lg text-left text-sm font-normal">
                     <tr>
@@ -94,7 +59,7 @@ export default async function WarehouseTable(query) {
                             </td>
                             <td className="whitespace-nowrap py-3 pl-6 pr-3">
                                 <div className="flex justify-end gap-3">
-
+                                    Edit Button Not Implemented Yet
                                 </div>
                             </td>
                         </tr>

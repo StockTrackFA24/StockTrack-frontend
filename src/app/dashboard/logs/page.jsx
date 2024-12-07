0f36f0e44f70eea9bb5aa67b6ce355c030182a1f
EricJurbergs <erj0008@uah.edu>
2024-12-07 14:31:05 -0600

    Audit log table is now working

--- src/app/dashboard/logs/page.jsx
+++ src/app/dashboard/logs/page.jsx
@@ -1,3 +1,22 @@
-export default function Page() {
-    return <p>Logs Page</p>;
+import Pagination from '@/app/ui/dashboard/pagination'
+import AuditTable from "@/app/ui/dashboard/logs/table";
+import {lusitana} from "@/app/fonts/next_fonts";
+import {findAuditPages} from "@/app/lib/data";
+
+export default async function Page(props) {
+    const searchParams = await props.searchParams;
+    const currentPage = Number(searchParams?.page) || 1;
+    const totalPages = await findAuditPages();
+    return (
+        <div className="w-full">
+            <div className="flex w-full items-center justify-between">
+                <h1 className={`${lusitana.className} text-2xl`}>Audit Logs</h1>
+            </div>
+            <AuditTable currentPage={currentPage} />
+            <div className="mt-5 flex w-full justify-center">
+                {<Pagination totalPages={totalPages} />
+                }
+            </div>
+        </div>
+    );
 }
--- src/app/lib/data.js
+++ src/app/lib/data.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import * as queryString from "node:querystring";
 
 const ITEMS_PER_PAGE = 6
 
@@ -16,7 +17,7 @@ export async function queryWarehouse(queryString, currentPage) {
     ).catch(function (error) {
         console.log(error);
     })
-    if (currentPage >= 0) {
+    if (currentPage >= 0 && responseData) {
         const offset = (currentPage - 1) * ITEMS_PER_PAGE
         const offset_end = offset + ITEMS_PER_PAGE;
         responseData = responseData.slice(offset, offset_end);
@@ -44,7 +45,7 @@ export async function queryBatches(queryString, currentPage) {
     ).catch(function (error) {
         console.log(error);
     })
-    if (currentPage >= 0) {
+    if (currentPage >= 0 && responseData) {
         const offset = (currentPage - 1) * ITEMS_PER_PAGE
         const offset_end = offset + ITEMS_PER_PAGE;
         responseData = responseData.slice(offset, offset_end);
@@ -56,4 +57,27 @@ export async function queryBatches(queryString, currentPage) {
 export async function findBatchPages(queryString) {
     let batchItems = await queryBatches(queryString, -1);
     return Math.ceil(batchItems.length / ITEMS_PER_PAGE);
+}
+
+export async function queryAuditLogs(currentPage) {
+    let address = process.env.BACKEND_ADDRESS + "/auditQuery";
+    let responseData;
+    await axios.post(address, {}, {
+        headers: {"Content-Type": "application/json"}
+    }).then(function (response) {
+        responseData = response.data;
+    }).catch(function (error) {
+        console.log(error);
+    })
+    if(currentPage >= 0 && responseData) {
+        const offset = (currentPage - 1) * ITEMS_PER_PAGE;
+        const offset_end = offset + ITEMS_PER_PAGE;
+        responseData = responseData.slice(offset, offset_end);
+    }
+    return responseData;
+}
+
+export async function findAuditPages() {
+    let auditItems = await queryAuditLogs(-1);
+    return Math.ceil(auditItems.length / ITEMS_PER_PAGE);
 }
